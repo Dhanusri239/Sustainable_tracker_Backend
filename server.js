3a06af1301084de6d70a222cefd5a46f91279337
Dhanusri239 <sdhanu946@gmail.com>
2025-10-09 22:07:45 +0530

    server file is updated

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+
 dotenv.config();
 connectDB();
-const app=express();
+
+const app = express();
+
 app.use(express.json());
 app.use(cors());
-app.get("/",(req,res)=>{
-    res.send("Sustainable tracker is running");
+
+// ✅ Make sure this line is exactly correct
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Sustainable tracker is running");
 });
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
- console.log("MongoDB is connected");
-})
-.catch((err)=>{
-    console.log("MongoDB is not connected",err);
-});
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log("server is running ");
-});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
